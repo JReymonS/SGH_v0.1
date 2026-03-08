9bfb8560fdf3279a2313d405aa7a8bbf337525ca
Ramon C.S <cedillojuanramon1d@gmail.com>
2026-03-08 12:17:42 -0600

    Agregar archivos de proyecto.

---
+++ AccesoDatos/Base.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace AccesoDatos
+{
+    public class Base
+    {
+        MySqlConnection conn;
+
+        //CONSTRUCTOR DE CONEXION
+        public Base(string servidor, string usuario, string password, string baseDatos, int port=3311)
+        {
+            conn = new MySqlConnection($"server={servidor}; user={usuario}; password={password}; database={baseDatos}; port={port};");
+        }
+
+        //REALIZAR UNA QUERY O COMANDO
+        public void Comando(string comando) 
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(comando, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"ERROR: Error al mandar comando.",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                conn.Close();
+            }
+        }
+
+        //REALIZAR UNA CONSULTA Y DEVOLVER DATOS
+        public DataSet Consulta(string consulta, string tabla) 
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter(consulta, conn);
+                conn.Open();
+                da.Fill(ds, tabla);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR: Error al realizar la consulta.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                conn.Close();
+            }
+            return ds;
+        }
+    }
+}
