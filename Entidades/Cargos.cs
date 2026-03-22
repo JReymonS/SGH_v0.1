ba36ff37a3bfc311545d8fa56282b11f472d1858
Ramon C.S <cedillojuanramon1d@gmail.com>
2026-03-21 23:48:30 -0600

    Extra: Se complementaron las entidades restantes con su estructura. (Falta definir algunas)

---
+++ Entidades/Cargos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cargos
+    {
+        //Agregar lo necesario para esta entidad...
+    }
+}
---
+++ Entidades/Reservas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Reservas
+    {
+        //Generar aqui lo necesario para la entidad...
+    }
+}
