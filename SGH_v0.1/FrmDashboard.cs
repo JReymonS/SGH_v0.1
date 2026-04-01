9e19b480bc3818868faaa9daae2ddd8035f2dd61
Diana Veloz <dvelozpadilla@gmail.com>
2026-04-01 15:04:10 -0600

    Cambiar la ejecución a FrmLogin desde el program.

--- SGH_v0.1/FrmDashboard.cs
+++ SGH_v0.1/FrmDashboard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace SGH_v0._1
 {
@@ -17,8 +18,28 @@ namespace SGH_v0._1
             InitializeComponent();
         }
 
+        private void Redondear(Control control, int radio)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radio, radio, 180, 90);
+            path.AddArc(control.Width - radio, 0, radio, radio, 270, 90);
+            path.AddArc(control.Width - radio, control.Height - radio, radio, radio, 0, 90);
+            path.AddArc(0, control.Height - radio, radio, radio, 90, 90);
+            path.CloseFigure();
+
+            control.Region = new Region(path);
+        }
+
         private void FrmDashboard_Load(object sender, EventArgs e)
         {
+            Redondear(panelHabitacionesDisponibles, 20);
+            Redondear(panelHabitacionesOcupadas, 20);
+            Redondear(panelHabitacionesLimpieza, 20);
+            Redondear(panelHabitacionesMantenimiento, 20);
+
+            Redondear(BtnNuevaReserva, 10);
+            Redondear(BtnHouseKeeping, 10);
+            Redondear(BtnReportes, 10);
 
         }
 
