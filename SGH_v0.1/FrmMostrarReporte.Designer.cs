1ddb930cabc68ddc69a79d87a37cb2afbdf97b6d
David AP <juandapssj@gmail.com>
2026-04-07 21:01:08 -0600

    Cambio tamanio de botones y posicion interfaz

--- SGH_v0.1/FrmMostrarReporte.Designer.cs
+++ SGH_v0.1/FrmMostrarReporte.Designer.cs
@@ -28,13 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMostrarReporte));
             this.DtgReporte = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnExportar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DtgReporte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // DtgReporte
@@ -42,33 +45,21 @@
             this.DtgReporte.AllowUserToAddRows = false;
             this.DtgReporte.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
             this.DtgReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.DtgReporte.DefaultCellStyle = dataGridViewCellStyle2;
-            this.DtgReporte.Location = new System.Drawing.Point(38, 74);
-            this.DtgReporte.Margin = new System.Windows.Forms.Padding(2);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DtgReporte.DefaultCellStyle = dataGridViewCellStyle1;
+            this.DtgReporte.Location = new System.Drawing.Point(51, 91);
+            this.DtgReporte.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DtgReporte.Name = "DtgReporte";
             this.DtgReporte.RowHeadersWidth = 51;
             this.DtgReporte.RowTemplate.Height = 24;
-            this.DtgReporte.Size = new System.Drawing.Size(750, 271);
+            this.DtgReporte.Size = new System.Drawing.Size(1000, 334);
             this.DtgReporte.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(145)))));
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(828, 61);
-            this.label1.TabIndex = 3;
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // btnExportar
             // 
@@ -76,10 +67,10 @@
             this.btnExportar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnExportar.FlatAppearance.BorderSize = 0;
             this.btnExportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExportar.Location = new System.Drawing.Point(676, 349);
+            this.btnExportar.Location = new System.Drawing.Point(850, 430);
             this.btnExportar.Margin = new System.Windows.Forms.Padding(0);
             this.btnExportar.Name = "btnExportar";
-            this.btnExportar.Size = new System.Drawing.Size(50, 50);
+            this.btnExportar.Size = new System.Drawing.Size(44, 44);
             this.btnExportar.TabIndex = 2;
             this.btnExportar.UseVisualStyleBackColor = false;
             this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
@@ -90,31 +81,70 @@
             this.btnCancelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnCancelar.FlatAppearance.BorderSize = 0;
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCancelar.Location = new System.Drawing.Point(742, 349);
+            this.btnCancelar.Location = new System.Drawing.Point(968, 430);
             this.btnCancelar.Margin = new System.Windows.Forms.Padding(0);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(50, 50);
+            this.btnCancelar.Size = new System.Drawing.Size(44, 44);
             this.btnCancelar.TabIndex = 1;
             this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(645, 446);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "EXPORTAR REPORTE:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(145)))));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::SGH_v0._1.Properties.Resources.Room_white_x64;
+            this.pictureBox1.Location = new System.Drawing.Point(1007, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(145)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1085, 75);
+            this.label3.TabIndex = 5;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // FrmMostrarReporte
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(825, 406);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1100, 500);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnExportar);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.DtgReporte);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FrmMostrarReporte";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmMostrarReporte";
             ((System.ComponentModel.ISupportInitialize)(this.DtgReporte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -123,6 +153,8 @@
         private System.Windows.Forms.DataGridView DtgReporte;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Button btnExportar;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- SGH_v0.1/FrmReportes.Designer.cs
+++ SGH_v0.1/FrmReportes.Designer.cs
@@ -52,7 +52,7 @@
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(826, 61);
+            this.label1.Size = new System.Drawing.Size(1085, 75);
             this.label1.TabIndex = 0;
             this.label1.Text = "REPORTE";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -61,10 +61,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(31, 113);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(41, 139);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 21);
+            this.label2.Size = new System.Drawing.Size(137, 28);
             this.label2.TabIndex = 2;
             this.label2.Text = "TIPO REPORTE";
             // 
@@ -76,10 +75,10 @@
             this.cmbReporte.Items.AddRange(new object[] {
             "General",
             "Especifico"});
-            this.cmbReporte.Location = new System.Drawing.Point(146, 110);
-            this.cmbReporte.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbReporte.Location = new System.Drawing.Point(195, 135);
+            this.cmbReporte.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbReporte.Name = "cmbReporte";
-            this.cmbReporte.Size = new System.Drawing.Size(207, 29);
+            this.cmbReporte.Size = new System.Drawing.Size(275, 36);
             this.cmbReporte.TabIndex = 3;
             this.cmbReporte.SelectedIndexChanged += new System.EventHandler(this.cmbReporte_SelectedIndexChanged);
             // 
@@ -87,10 +86,9 @@
             // 
             this.lblFechaInicial.AutoSize = true;
             this.lblFechaInicial.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFechaInicial.Location = new System.Drawing.Point(31, 235);
-            this.lblFechaInicial.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFechaInicial.Location = new System.Drawing.Point(41, 276);
             this.lblFechaInicial.Name = "lblFechaInicial";
-            this.lblFechaInicial.Size = new System.Drawing.Size(109, 21);
+            this.lblFechaInicial.Size = new System.Drawing.Size(135, 28);
             this.lblFechaInicial.TabIndex = 4;
             this.lblFechaInicial.Text = "FECHA INCIAL";
             this.lblFechaInicial.Visible = false;
@@ -98,10 +96,10 @@
             // dtpFechaReporteIni
             // 
             this.dtpFechaReporteIni.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpFechaReporteIni.Location = new System.Drawing.Point(144, 230);
-            this.dtpFechaReporteIni.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpFechaReporteIni.Location = new System.Drawing.Point(192, 270);
+            this.dtpFechaReporteIni.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtpFechaReporteIni.Name = "dtpFechaReporteIni";
-            this.dtpFechaReporteIni.Size = new System.Drawing.Size(243, 29);
+            this.dtpFechaReporteIni.Size = new System.Drawing.Size(323, 34);
             this.dtpFechaReporteIni.TabIndex = 5;
             this.dtpFechaReporteIni.Visible = false;
             // 
@@ -110,10 +108,10 @@
             this.cmbEspecifico.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbEspecifico.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbEspecifico.FormattingEnabled = true;
-            this.cmbEspecifico.Location = new System.Drawing.Point(458, 107);
-            this.cmbEspecifico.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbEspecifico.Location = new System.Drawing.Point(611, 132);
+            this.cmbEspecifico.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbEspecifico.Name = "cmbEspecifico";
-            this.cmbEspecifico.Size = new System.Drawing.Size(207, 29);
+            this.cmbEspecifico.Size = new System.Drawing.Size(275, 36);
             this.cmbEspecifico.TabIndex = 8;
             this.cmbEspecifico.Visible = false;
             // 
@@ -121,10 +119,9 @@
             // 
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(379, 110);
-            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Location = new System.Drawing.Point(505, 135);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(75, 21);
+            this.lblNombre.Size = new System.Drawing.Size(93, 28);
             this.lblNombre.TabIndex = 9;
             this.lblNombre.Text = "NOMBRE";
             this.lblNombre.Visible = false;
@@ -132,10 +129,10 @@
             // dtpFechaReporteFini
             // 
             this.dtpFechaReporteFini.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpFechaReporteFini.Location = new System.Drawing.Point(511, 230);
-            this.dtpFechaReporteFini.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpFechaReporteFini.Location = new System.Drawing.Point(681, 270);
+            this.dtpFechaReporteFini.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtpFechaReporteFini.Name = "dtpFechaReporteFini";
-            this.dtpFechaReporteFini.Size = new System.Drawing.Size(243, 29);
+            this.dtpFechaReporteFini.Size = new System.Drawing.Size(323, 34);
             this.dtpFechaReporteFini.TabIndex = 11;
             this.dtpFechaReporteFini.Visible = false;
             // 
@@ -143,10 +140,9 @@
             // 
             this.lblFechaFinal.AutoSize = true;
             this.lblFechaFinal.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFechaFinal.Location = new System.Drawing.Point(404, 235);
-            this.lblFechaFinal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFechaFinal.Location = new System.Drawing.Point(539, 276);
             this.lblFechaFinal.Name = "lblFechaFinal";
-            this.lblFechaFinal.Size = new System.Drawing.Size(103, 21);
+            this.lblFechaFinal.Size = new System.Drawing.Size(128, 28);
             this.lblFechaFinal.TabIndex = 10;
             this.lblFechaFinal.Text = "FECHA FINAL";
             this.lblFechaFinal.Visible = false;
@@ -157,10 +153,10 @@
             this.btnCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnCerrar.FlatAppearance.BorderSize = 0;
             this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCerrar.Location = new System.Drawing.Point(693, 325);
+            this.btnCerrar.Location = new System.Drawing.Point(968, 430);
             this.btnCerrar.Margin = new System.Windows.Forms.Padding(0);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(50, 50);
+            this.btnCerrar.Size = new System.Drawing.Size(44, 44);
             this.btnCerrar.TabIndex = 7;
             this.btnCerrar.UseVisualStyleBackColor = false;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
@@ -172,10 +168,10 @@
             this.btnBuscar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnBuscar.FlatAppearance.BorderSize = 0;
             this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBuscar.Location = new System.Drawing.Point(589, 325);
+            this.btnBuscar.Location = new System.Drawing.Point(850, 430);
             this.btnBuscar.Margin = new System.Windows.Forms.Padding(0);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(50, 50);
+            this.btnBuscar.Size = new System.Drawing.Size(44, 44);
             this.btnBuscar.TabIndex = 6;
             this.btnBuscar.UseVisualStyleBackColor = false;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
@@ -185,20 +181,20 @@
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(145)))));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::SGH_v0._1.Properties.Resources.Room_white_x64;
-            this.pictureBox1.Location = new System.Drawing.Point(765, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(1007, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(49, 61);
+            this.pictureBox1.Size = new System.Drawing.Size(65, 75);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // FrmReportes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(825, 406);
+            this.ClientSize = new System.Drawing.Size(1100, 500);
             this.Controls.Add(this.dtpFechaReporteFini);
             this.Controls.Add(this.lblFechaFinal);
             this.Controls.Add(this.lblNombre);
@@ -212,7 +208,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FrmReportes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmReportes";
