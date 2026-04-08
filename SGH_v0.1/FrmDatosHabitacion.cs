6d672668896a1198ed4801030d2489fa5c3d1c2f
QuotedCris34 <cristian2005salazar@gmail.com>
2026-04-07 20:57:06 -0600

    Modulo de habitaciones, con diseño de reservas (solo diseño)
    Login home

---
+++ SGH_v0.1/FrmDatosHabitacion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Manejadores;
+using Entidades;
+
+namespace SGH_v0._1
+{
+    public partial class FrmDatosHabitacion : Form
+    {
+        ManejadorHabitaciones mh;
+
+        public FrmDatosHabitacion()
+        {
+            InitializeComponent();
+            mh = new ManejadorHabitaciones();
+            var h = FrmHabitaciones.habitacionSeleccionada;
+
+            if (h != null && !string.IsNullOrEmpty(h.Numero_Habitacion))
+            {
+                TxtNo.Text = h.Numero_Habitacion;
+                TxtTipo.Text = h.Tipo_Habitacion;
+                TxtCapacidad.Text = h.Capacidad.ToString();
+                TxtPiso.Text = h.Piso.ToString();
+                TxtCosto.Text = h.Costo_Noche.ToString();
+                TxtDescripcion.Text = h.Descripcion;
+            }
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            Habitaciones h = new Habitaciones();
+            h.Numero_Habitacion = TxtNo.Text;
+            h.Tipo_Habitacion = TxtTipo.Text;
+            h.Capacidad = int.Parse(TxtCapacidad.Text);
+            h.Piso = int.Parse(TxtPiso.Text);
+            h.Costo_Noche = double.Parse(TxtCosto.Text);
+            h.Descripcion = TxtDescripcion.Text;
+
+            if (FrmHabitaciones.habitacionSeleccionada == null)
+            {
+                mh.Guardar(h);
+            }
+            else
+            {
+                mh.Modificar(h, FrmHabitaciones.habitacionSeleccionada.Numero_Habitacion);
+            }
+
+            this.Close();
+        }
+    }
+}
---
+++ SGH_v0.1/FrmHabitaciones.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+using Manejadores;
+
+namespace SGH_v0._1
+{
+    public partial class FrmHabitaciones : Form
+    {
+        ManejadorHabitaciones mh;
+        public static Habitaciones habitacion = new Habitaciones("","",0,0,0,"","",0);
+        int fila = 0, columna = 0;
+        public static Habitaciones habitacionSeleccionada;
+        public static string numeroActual;
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            mh.Mostrar($"SELECT * FROM v_Habitaciones;", DtgDatos, "Habitaciones");
+        }
+
+        private void DtgDatos_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            fila = e.RowIndex; columna = e.ColumnIndex;
+        }
+
+        private void DtgDatos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (fila < 0) return; // evita error si clic en encabezado
+
+            habitacion.Numero_Habitacion = DtgDatos.Rows[fila].Cells[0].Value.ToString();
+            habitacion.Tipo_Habitacion = DtgDatos.Rows[fila].Cells[1].Value.ToString();
+            habitacion.Descripcion = DtgDatos.Rows[fila].Cells[2].Value.ToString();
+            habitacion.Capacidad = int.Parse(DtgDatos.Rows[fila].Cells[3].Value.ToString());
+            habitacion.Piso = int.Parse(DtgDatos.Rows[fila].Cells[4].Value.ToString());
+
+            switch (columna)
+            {
+                case 5:
+                    {
+                        FrmReservas fr = new FrmReservas();
+                        fr.ShowDialog();
+                        DtgDatos.Columns.Clear();
+                    }; break;
+                case 6:
+                    {
+                        //CHECK IN
+                    }; break;
+                case 7:
+                    {
+                        //CHECK OUT
+                    }; break;
+            }
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            FrmHabitaciones.habitacion = null;
+            new FrmDatosHabitacion().ShowDialog();
+            DtgDatos.Columns.Clear();
+        }
+
+        private void BtnEditar_Click(object sender, EventArgs e)
+        {
+            if (DtgDatos.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione una habitación primero.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string numero = DtgDatos.SelectedRows[0].Cells[0].Value.ToString();
+
+            FrmHabitaciones.habitacionSeleccionada = mh.ObtenerHabitacion(numero);
+
+            new FrmDatosHabitacion().ShowDialog();
+
+            DtgDatos.Columns.Clear();
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            if (DtgDatos.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione una habitación primero.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string numero = DtgDatos.SelectedRows[0].Cells["NO"].Value.ToString();
+            habitacion.Numero_Habitacion = numero;
+
+            mh.Borrar(habitacion);
+
+            DtgDatos.Columns.Clear();
+        }
+
+        public FrmHabitaciones()
+        {
+            InitializeComponent();
+            mh = new ManejadorHabitaciones();
+        }
+    }
+}
