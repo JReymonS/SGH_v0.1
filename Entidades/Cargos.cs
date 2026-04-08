c543b147d6d3d0f2d4aba0f066c9689328d87de1
Diana Veloz <dvelozpadilla@gmail.com>
2026-04-08 15:04:20 -0600

    Agregar propiedades de entidad Cargos.

--- Entidades/Cargos.cs
+++ Entidades/Cargos.cs
@@ -8,6 +8,20 @@ namespace Entidades
 {
     public class Cargos
     {
-        //Agregar lo necesario para esta entidad...
+        public Cargos(int id_Cargo, string fecha, string concepto, decimal monto, int id_Reserva)
+        {
+            Id_Cargo = id_Cargo;
+            Fecha = fecha;
+            Concepto = concepto;
+            Monto = monto;
+            Id_Reserva = id_Reserva;
+        }
+
+        public int Id_Cargo { get; set; }
+        public string Fecha { get; set; }
+        public string Concepto { get; set; }
+        public decimal Monto { get; set; }
+        public int Id_Reserva { get; set; }
+
     }
 }
