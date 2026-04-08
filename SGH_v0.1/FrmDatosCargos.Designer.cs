ee06158dc23bf4a6ece470e86911c37cbe01a645
Diana Veloz <dvelozpadilla@gmail.com>
2026-04-08 09:14:55 -0600

    Implementación de diseño en FrmDatosCargos.

--- SGH_v0.1/FrmDatosCargos.Designer.cs
+++ SGH_v0.1/FrmDatosCargos.Designer.cs
@@ -28,19 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtDesayuno = new System.Windows.Forms.TextBox();
+            this.TxtCostoDesayuno = new System.Windows.Forms.TextBox();
+            this.TxtBebida = new System.Windows.Forms.TextBox();
+            this.TxtDescripcion = new System.Windows.Forms.TextBox();
+            this.TxtCostoBebida = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(145)))));
+            this.label1.Font = new System.Drawing.Font("Montserrat SemiBold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(823, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AGREGAR CARGOS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(145)))));
+            this.pictureBox1.BackgroundImage = global::SGH_v0._1.Properties.Resources.Room_white;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(752, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CARGO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(634, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "COSTO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(449, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "BEBIDA:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(255, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "COSTO:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(60, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "DESCRIPCIÓN:";
+            // 
+            // TxtDesayuno
+            // 
+            this.TxtDesayuno.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtDesayuno.Enabled = false;
+            this.TxtDesayuno.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtDesayuno.Location = new System.Drawing.Point(120, 121);
+            this.TxtDesayuno.Multiline = true;
+            this.TxtDesayuno.Name = "TxtDesayuno";
+            this.TxtDesayuno.Size = new System.Drawing.Size(100, 20);
+            this.TxtDesayuno.TabIndex = 7;
+            this.TxtDesayuno.Text = "Desayuno";
+            this.TxtDesayuno.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtCostoDesayuno
+            // 
+            this.TxtCostoDesayuno.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtCostoDesayuno.Enabled = false;
+            this.TxtCostoDesayuno.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtCostoDesayuno.Location = new System.Drawing.Point(313, 121);
+            this.TxtCostoDesayuno.Multiline = true;
+            this.TxtCostoDesayuno.Name = "TxtCostoDesayuno";
+            this.TxtCostoDesayuno.Size = new System.Drawing.Size(100, 20);
+            this.TxtCostoDesayuno.TabIndex = 8;
+            this.TxtCostoDesayuno.Text = "150.00";
+            this.TxtCostoDesayuno.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtBebida
+            // 
+            this.TxtBebida.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtBebida.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBebida.Location = new System.Drawing.Point(507, 119);
+            this.TxtBebida.Multiline = true;
+            this.TxtBebida.Name = "TxtBebida";
+            this.TxtBebida.Size = new System.Drawing.Size(100, 20);
+            this.TxtBebida.TabIndex = 9;
+            this.TxtBebida.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtDescripcion
+            // 
+            this.TxtDescripcion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtDescripcion.Enabled = false;
+            this.TxtDescripcion.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtDescripcion.Location = new System.Drawing.Point(157, 172);
+            this.TxtDescripcion.Multiline = true;
+            this.TxtDescripcion.Name = "TxtDescripcion";
+            this.TxtDescripcion.Size = new System.Drawing.Size(175, 39);
+            this.TxtDescripcion.TabIndex = 10;
+            this.TxtDescripcion.Text = "Huevos estrellados con chilaquiles rojos, frijoles ballos y un bolillo.\r\n";
+            this.TxtDescripcion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtCostoBebida
+            // 
+            this.TxtCostoBebida.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtCostoBebida.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtCostoBebida.Location = new System.Drawing.Point(692, 120);
+            this.TxtCostoBebida.Multiline = true;
+            this.TxtCostoBebida.Name = "TxtCostoBebida";
+            this.TxtCostoBebida.Size = new System.Drawing.Size(100, 20);
+            this.TxtCostoBebida.TabIndex = 11;
+            this.TxtCostoBebida.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // FrmDatosCargos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(857, 281);
+            this.Controls.Add(this.TxtCostoBebida);
+            this.Controls.Add(this.TxtDescripcion);
+            this.Controls.Add(this.TxtBebida);
+            this.Controls.Add(this.TxtCostoDesayuno);
+            this.Controls.Add(this.TxtDesayuno);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmDatosCargos";
             this.Text = "FrmDatosCargos";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtDesayuno;
+        private System.Windows.Forms.TextBox TxtCostoDesayuno;
+        private System.Windows.Forms.TextBox TxtBebida;
+        private System.Windows.Forms.TextBox TxtDescripcion;
+        private System.Windows.Forms.TextBox TxtCostoBebida;
     }
 }
