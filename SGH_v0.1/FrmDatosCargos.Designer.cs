8ebe086733050cfdefb94b926294f5d99d7b2404
Diana Veloz <dvelozpadilla@gmail.com>
2026-04-08 14:35:04 -0600

    Se cambio el formulario inicial desde el program para que al compilar pueda abrir FrmDatosCargos y hacer prubas (solo de Diseño).
    Como detalle extra, se cambio el fondo del formulario a Blanco y la posicion al compilar.

--- SGH_v0.1/FrmDatosCargos.Designer.cs
+++ SGH_v0.1/FrmDatosCargos.Designer.cs
@@ -208,6 +208,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(857, 281);
             this.Controls.Add(this.BtnCerrar);
             this.Controls.Add(this.BtnGuardar);
@@ -225,6 +226,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmDatosCargos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmDatosCargos";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
