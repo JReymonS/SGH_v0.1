64c40a09ac51be15b92c0da6a70d8769302f8da2
Diana Veloz <dvelozpadilla@gmail.com>
2026-04-08 18:57:34 -0600

    Creación de ManejadorCargos e integración de la conexión del maismo con la Base de Datos.

---
+++ Manejadores/ManejadorCargos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+using Entidades;
+
+namespace Manejadores
+{
+    public class ManejadorCargos
+    {
+        Base b = new Base("localhost", "root", "2026", "SistemaGestionHotelera", 3311);
+
+    }
+}
