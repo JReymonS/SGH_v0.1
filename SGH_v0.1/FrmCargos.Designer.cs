0d578329d31b9d3b801eeec8d789c83682eb1467
Diana Veloz <dvelozpadilla@gmail.com>
2026-04-20 07:27:55 -0600

    Se elimino un evento generado por accidente.

--- SGH_v0.1/FrmCargos.Designer.cs
+++ SGH_v0.1/FrmCargos.Designer.cs
@@ -56,10 +56,9 @@
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(145)))));
             this.label1.Font = new System.Drawing.Font("Montserrat SemiBold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(-1, 31);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(-1, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1001, 80);
+            this.label1.Size = new System.Drawing.Size(751, 65);
             this.label1.TabIndex = 0;
             this.label1.Text = "CARGOS A LA HABITACIÓN";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -70,10 +69,9 @@
             this.pictureBox1.BackgroundImage = global::SGH_v0._1.Properties.Resources.Room_white;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.ImageLocation = "";
-            this.pictureBox1.Location = new System.Drawing.Point(867, 46);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(650, 37);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(53, 49);
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -81,21 +79,19 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(47, 148);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(35, 120);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 23);
+            this.label2.Size = new System.Drawing.Size(66, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "HUESPED:";
             // 
             // TxtHuesped
             // 
             this.TxtHuesped.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.TxtHuesped.Location = new System.Drawing.Point(143, 144);
-            this.TxtHuesped.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtHuesped.Location = new System.Drawing.Point(107, 117);
             this.TxtHuesped.Multiline = true;
             this.TxtHuesped.Name = "TxtHuesped";
-            this.TxtHuesped.Size = new System.Drawing.Size(647, 29);
+            this.TxtHuesped.Size = new System.Drawing.Size(486, 24);
             this.TxtHuesped.TabIndex = 3;
             // 
             // DtgDatosHuesped
@@ -114,11 +110,10 @@
             this.DtgDatosHuesped.DefaultCellStyle = dataGridViewCellStyle1;
             this.DtgDatosHuesped.GridColor = System.Drawing.SystemColors.ControlText;
             this.DtgDatosHuesped.ImeMode = System.Windows.Forms.ImeMode.Katakana;
-            this.DtgDatosHuesped.Location = new System.Drawing.Point(48, 191);
-            this.DtgDatosHuesped.Margin = new System.Windows.Forms.Padding(4);
+            this.DtgDatosHuesped.Location = new System.Drawing.Point(36, 155);
             this.DtgDatosHuesped.Name = "DtgDatosHuesped";
             this.DtgDatosHuesped.RowHeadersWidth = 51;
-            this.DtgDatosHuesped.Size = new System.Drawing.Size(833, 153);
+            this.DtgDatosHuesped.Size = new System.Drawing.Size(625, 124);
             this.DtgDatosHuesped.TabIndex = 5;
             this.DtgDatosHuesped.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatosHuesped_CellClick);
             // 
@@ -137,11 +132,10 @@
             this.DtgDatosCargo.DefaultCellStyle = dataGridViewCellStyle2;
             this.DtgDatosCargo.GridColor = System.Drawing.SystemColors.ControlText;
             this.DtgDatosCargo.ImeMode = System.Windows.Forms.ImeMode.Katakana;
-            this.DtgDatosCargo.Location = new System.Drawing.Point(48, 370);
-            this.DtgDatosCargo.Margin = new System.Windows.Forms.Padding(4);
+            this.DtgDatosCargo.Location = new System.Drawing.Point(36, 301);
             this.DtgDatosCargo.Name = "DtgDatosCargo";
             this.DtgDatosCargo.RowHeadersWidth = 51;
-            this.DtgDatosCargo.Size = new System.Drawing.Size(577, 139);
+            this.DtgDatosCargo.Size = new System.Drawing.Size(433, 113);
             this.DtgDatosCargo.TabIndex = 6;
             this.DtgDatosCargo.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatosCargo_CellClick);
             this.DtgDatosCargo.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatosCargo_CellEnter);
@@ -152,10 +146,9 @@
             this.BtnBuscar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.BtnBuscar.FlatAppearance.BorderSize = 0;
             this.BtnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnBuscar.Location = new System.Drawing.Point(789, 134);
-            this.BtnBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnBuscar.Location = new System.Drawing.Point(592, 109);
             this.BtnBuscar.Name = "BtnBuscar";
-            this.BtnBuscar.Size = new System.Drawing.Size(53, 49);
+            this.BtnBuscar.Size = new System.Drawing.Size(40, 40);
             this.BtnBuscar.TabIndex = 7;
             this.BtnBuscar.UseVisualStyleBackColor = true;
             this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
@@ -166,10 +159,9 @@
             this.BtnAgregarCargo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.BtnAgregarCargo.FlatAppearance.BorderSize = 0;
             this.BtnAgregarCargo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnAgregarCargo.Location = new System.Drawing.Point(633, 362);
-            this.BtnAgregarCargo.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnAgregarCargo.Location = new System.Drawing.Point(475, 294);
             this.BtnAgregarCargo.Name = "BtnAgregarCargo";
-            this.BtnAgregarCargo.Size = new System.Drawing.Size(53, 49);
+            this.BtnAgregarCargo.Size = new System.Drawing.Size(40, 40);
             this.BtnAgregarCargo.TabIndex = 8;
             this.BtnAgregarCargo.UseVisualStyleBackColor = true;
             this.BtnAgregarCargo.Click += new System.EventHandler(this.BtnAgregarCargo_Click_1);
@@ -178,10 +170,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(716, 375);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(537, 305);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 23);
+            this.label3.Size = new System.Drawing.Size(59, 17);
             this.label3.TabIndex = 9;
             this.label3.Text = "MONTO:";
             // 
@@ -190,11 +181,10 @@
             this.TxtMonto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.TxtMonto.Enabled = false;
             this.TxtMonto.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtMonto.Location = new System.Drawing.Point(799, 370);
-            this.TxtMonto.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtMonto.Location = new System.Drawing.Point(599, 301);
             this.TxtMonto.Multiline = true;
             this.TxtMonto.Name = "TxtMonto";
-            this.TxtMonto.Size = new System.Drawing.Size(81, 28);
+            this.TxtMonto.Size = new System.Drawing.Size(61, 23);
             this.TxtMonto.TabIndex = 10;
             this.TxtMonto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -202,10 +192,9 @@
             // 
             this.pictureBox2.BackgroundImage = global::SGH_v0._1.Properties.Resources.Info;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(53, 551);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(40, 448);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(27, 25);
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
             this.pictureBox2.TabIndex = 11;
             this.pictureBox2.TabStop = false;
             // 
@@ -213,10 +202,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(81, 554);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(61, 450);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(339, 38);
+            this.label4.Size = new System.Drawing.Size(288, 26);
             this.label4.TabIndex = 12;
             this.label4.Text = "Tip: Seleccione un registro que desea editar o eliminar.\r\n\r\n";
             // 
@@ -226,10 +214,9 @@
             this.BtnPagar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.BtnPagar.FlatAppearance.BorderSize = 0;
             this.BtnPagar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnPagar.Location = new System.Drawing.Point(799, 530);
-            this.BtnPagar.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnPagar.Location = new System.Drawing.Point(599, 431);
             this.BtnPagar.Name = "BtnPagar";
-            this.BtnPagar.Size = new System.Drawing.Size(57, 49);
+            this.BtnPagar.Size = new System.Drawing.Size(43, 40);
             this.BtnPagar.TabIndex = 13;
             this.BtnPagar.UseVisualStyleBackColor = true;
             // 
@@ -239,10 +226,9 @@
             this.BtnEditar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.BtnEditar.FlatAppearance.BorderSize = 0;
             this.BtnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnEditar.Location = new System.Drawing.Point(873, 528);
-            this.BtnEditar.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnEditar.Location = new System.Drawing.Point(655, 429);
             this.BtnEditar.Name = "BtnEditar";
-            this.BtnEditar.Size = new System.Drawing.Size(53, 49);
+            this.BtnEditar.Size = new System.Drawing.Size(40, 40);
             this.BtnEditar.TabIndex = 14;
             this.BtnEditar.UseVisualStyleBackColor = true;
             this.BtnEditar.Click += new System.EventHandler(this.BtnEditar_Click_1);
@@ -253,20 +239,19 @@
             this.BtnEliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.BtnEliminar.FlatAppearance.BorderSize = 0;
             this.BtnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnEliminar.Location = new System.Drawing.Point(944, 529);
-            this.BtnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnEliminar.Location = new System.Drawing.Point(708, 430);
             this.BtnEliminar.Name = "BtnEliminar";
-            this.BtnEliminar.Size = new System.Drawing.Size(53, 49);
+            this.BtnEliminar.Size = new System.Drawing.Size(40, 40);
             this.BtnEliminar.TabIndex = 15;
             this.BtnEliminar.UseVisualStyleBackColor = true;
             this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click_1);
             // 
             // FrmCargos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1035, 603);
+            this.ClientSize = new System.Drawing.Size(776, 490);
             this.Controls.Add(this.TxtHuesped);
             this.Controls.Add(this.BtnEliminar);
             this.Controls.Add(this.BtnEditar);
@@ -283,10 +268,10 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmCargos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmCargos";
+//            this.Load += new System.EventHandler(this.FrmCargos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DtgDatosHuesped)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DtgDatosCargo)).EndInit();
