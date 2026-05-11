d9688b3ff95211666d8248101dc0004528690710
Diana Veloz <dvelozpadilla@gmail.com>
2026-05-11 08:54:24 -0600

    Se agregaron iconos de distintos pixeles para los modulos en la carpeta de Resources.

--- SGH_v0.1/Properties/Resources.Designer.cs
+++ SGH_v0.1/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SGH_v0._1.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -431,12 +431,52 @@ namespace SGH_v0._1.Properties {
         }
         
         /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        internal static byte[] LOGOTIPO_FONDO128 {
+            get {
+                object obj = ResourceManager.GetObject("LOGOTIPO_FONDO128", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap LOGOTIPO_FONDO2 {
             get {
                 object obj = ResourceManager.GetObject("LOGOTIPO_FONDO2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        internal static byte[] LOGOTIPO_FONDO256 {
+            get {
+                object obj = ResourceManager.GetObject("LOGOTIPO_FONDO256", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        internal static byte[] LOGOTIPO_FONDO32 {
+            get {
+                object obj = ResourceManager.GetObject("LOGOTIPO_FONDO32", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        internal static byte[] LOGOTIPO_FONDO64 {
+            get {
+                object obj = ResourceManager.GetObject("LOGOTIPO_FONDO64", resourceCulture);
+                return ((byte[])(obj));
             }
         }
         
