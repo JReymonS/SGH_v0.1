59c57cf2d4db208e9a54a6b6e488a9bd8485bec6
Diana Veloz <dvelozpadilla@gmail.com>
2026-05-11 19:16:53 -0600

    Se agrego icono al modulo Datos Usuario.

--- SGH_v0.1/FrmDatosUsuario.Designer.cs
+++ SGH_v0.1/FrmDatosUsuario.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDatosUsuario));
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -218,6 +219,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmDatosUsuario";
