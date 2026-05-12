a84382e61558fd6c2bbaafa5c2b57f6ca230d0cc
Diana Veloz <dvelozpadilla@gmail.com>
2026-05-11 19:26:53 -0600

    Se agrego el icono x64 de CloudInn al módulo Permisos.

--- SGH_v0.1/FrmPermisosUsuario.Designer.cs
+++ SGH_v0.1/FrmPermisosUsuario.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPermisosUsuario));
             this.pEncabezado = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -270,6 +271,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pEncabezado);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmPermisosUsuario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Permisos";
