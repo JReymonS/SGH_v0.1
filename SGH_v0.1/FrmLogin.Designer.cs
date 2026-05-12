30f7f51bd8f63761fd6a644a3a39006093ad7dd0
Diana Veloz <dvelozpadilla@gmail.com>
2026-05-11 19:23:32 -0600

    Se agrego el icono x64 de CloudInn al módulo Login  y se inicializó FrmLogin para pruebas.

--- SGH_v0.1/FrmLogin.Designer.cs
+++ SGH_v0.1/FrmLogin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -192,6 +193,7 @@
             this.ClientSize = new System.Drawing.Size(500, 646);
             this.Controls.Add(this.PLogin);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmLogin";
